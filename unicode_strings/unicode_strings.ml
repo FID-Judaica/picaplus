d0e072f23774da75a389aa37b5f73afb5ebe19fa
Aaron Christianson <ninjaaron@gmail.com>
2020-05-22 18:12:44 +0200

    adding a module for working with unicode strings as arrays of ints

---
+++ unicode_strings/unicode_strings.ml
@@ -0,0 +1,12 @@
+let update_buff_from_string ?encoding ~buf s =
+  let decoder = Uutf.decoder ?encoding s in
+  let rec loop n = function
+    | `End -> Ok (n+1)
+    | `Await -> failwith "shouldn't be able to get here"
+    | `Malformed s -> Error s
+    | `Uchar c ->
+       buf.(n) <- Uchar.to_int c;
+       loop (n+1) (Uutf.decode decoder) in
+  loop 0 (Uutf.decode decoder)
+
+  
