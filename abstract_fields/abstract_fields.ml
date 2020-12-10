1f993949ce0b5211276b1e136b7e4e020ede1554
Aaron Christianson <ninjaaron@gmail.com>
2020-12-10 19:35:21 +0100

    formatting stuff

--- abstract_fields/abstract_fields.ml
+++ abstract_fields/abstract_fields.ml
@@ -1,4 +1,5 @@
 open Base
+
 let ws = Re.Perl.compile_pat "^\\s*(.*)\\s*$"
 let strip_whitespace s = Re.replace ws ~f:(fun g -> Re.Group.get g 1) s
 
@@ -8,56 +9,69 @@ let rec any ~f = function
 ;;
 
 module Title = struct
-  type t = { main: string
-           ; sub: string option
-           ; name: string option
-           ; script: string
-           } [@@deriving sexp]
-  ;;
-  let make ~main ~sub ~name ~script = {main; sub; name; script}
+  type t =
+    { main : string
+    ; sub : string option
+    ; name : string option
+    ; script : string
+    }
+  [@@deriving sexp]
+
+  let make ~main ~sub ~name ~script = { main; sub; name; script }
 
   let repr tl sep = function
     | None -> tl
     | Some s -> sep :: s :: tl
-  let repr {main; sub; name; _} =
+  ;;
+
+  let repr { main; sub; name; _ } =
     let tl = repr [] "/" name in
     let tl = repr tl ":" sub in
     String.concat ~sep:" " (main :: tl)
   ;;
-  let has_hebrew_script titles =
-    any ~f:(fun t -> String.equal t.script "Hebr") titles
+
+  let has_hebrew_script titles = any ~f:(fun t -> String.equal t.script "Hebr") titles
 end
 
 module Person = struct
-  type t = { name: string
-           ; first_name: string option
-           ; identifiers: string list
-           ; script: string
-           } [@@deriving sexp]
-  ;;
+  type t =
+    { name : string
+    ; first_name : string option
+    ; identifiers : string list
+    ; script : string
+    }
+  [@@deriving sexp]
+
   let make ?first_name ~name ~identifiers ~script =
-    {name; first_name; identifiers; script}
+    { name; first_name; identifiers; script }
+  ;;
+
   let or_name t ~f =
     match t.first_name with
     | None -> t.name
     | Some fn -> f fn t.name
-  let natural_name =
-    or_name ~f:(fun fn name -> String.concat ~sep:" " [fn; name])
-  let comma_name =
-    or_name ~f:(fun fn name -> String.concat ~sep:", " [name; fn])
+  ;;
+
+  let natural_name = or_name ~f:(fun fn name -> String.concat ~sep:" " [ fn; name ])
+  let comma_name = or_name ~f:(fun fn name -> String.concat ~sep:", " [ name; fn ])
+
   let comma_split t =
     match t.name, t.first_name with
     | _, Some _ -> Ok t
     | s, _ ->
-       match String.split s ~on:',' |> List.map ~f:strip_whitespace with
-       | [name; fn] -> Ok {t with name; first_name = Some fn}
-       | _ -> Error t
+      (match String.split s ~on:',' |> List.map ~f:strip_whitespace with
+      | [ name; fn ] -> Ok { t with name; first_name = Some fn }
+      | _ -> Error t)
+  ;;
 end
 
 module Publisher = struct
-  type t = { name: string option
-           ; place: string list
-           ; script: string
-           } [@@deriving sexp]
-  let make ?name ~place ~script = {name; place; script}
+  type t =
+    { name : string option
+    ; place : string list
+    ; script : string
+    }
+  [@@deriving sexp]
+
+  let make ?name ~place ~script = { name; place; script }
 end
